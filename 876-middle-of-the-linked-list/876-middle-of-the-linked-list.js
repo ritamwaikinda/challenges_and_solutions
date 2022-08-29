fe7b96ff2d24116baa66abd221b7c5a356dc47f3
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2022-08-29 13:10:12 -0400

    Time: 62 ms (93.33%), Space: 41.3 MB (99.11%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -12,12 +12,20 @@
 var middleNode = function(head) {
     let current = head;
     let middle = head;
-    while(current.next !== null){
-        current = current.next
+    while(current !== null && current.next !== null){
+        current = current.next.next
         middle = middle.next
-        if(current.next !== null){
-            current = current.next
-        }
+        // if(current.next !== null){
+        //     current = current.next
+        // }
     }  
+    
+    /*
+    You can land on a null value. You wont be able to go any further, but you can LAND on it.
+    while (current != null (if you land on a null, it can't read current.next) && current.next !== null)
+    
+    
+    */
+    
     return middle
 };
