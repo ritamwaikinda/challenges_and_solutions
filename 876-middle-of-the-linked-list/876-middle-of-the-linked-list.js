8652e45dc3533b9c3fdb6e23606c3df158bad40e
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2022-08-29 12:49:35 -0400

    Time: 66 ms (88.12%), Space: 41.6 MB (95.52%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -10,17 +10,14 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let start = 0;
     let current = head;
     let middle = head;
     while(current.next !== null){
-        start += 1
         current = current.next
-    }
-    
-    for(let i=0; i<Math.ceil(start/2); i++){
-        middle = middle.next;
-    }
-    
+        middle = middle.next
+        if(current.next !== null){
+            current = current.next
+        }
+    }  
     return middle
 };
