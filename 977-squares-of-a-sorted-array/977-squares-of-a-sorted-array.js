88e0d356012772977a1969fa57153846fb1445c3
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2022-09-04 20:18:10 -0400

    Time: 198 ms (19.40%), Space: 48.7 MB (49.06%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,22 +2,30 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var sortedSquares = function(nums) {
+//     // for(let i=0; i<nums.length; i++){
+//     //     nums[i] = nums[i] ** 2
+//     // }
+//     // return nums.sort((a,b) => a-b)
+//     let squarednums = []
+//     let left = 0;
+//     let right = nums.length - 1;
+//     while (left <= right){
+//         if (Math.abs(nums[left]) > Math.abs(nums[right])){
+//             squarednums.unshift(nums[left] ** 2)
+//             left ++
+//         } else {
+//             squarednums.unshift(nums[right] ** 2)
+//             right --
+//         }
+//     }
+//     return squarednums
+// };
+
 var sortedSquares = function(nums) {
-    // for(let i=0; i<nums.length; i++){
-    //     nums[i] = nums[i] ** 2
-    // }
-    // return nums.sort((a,b) => a-b)
-    let squarednums = []
-    let left = 0;
-    let right = nums.length - 1;
-    while (left <= right){
-        if (Math.abs(nums[left]) > Math.abs(nums[right])){
-            squarednums.unshift(nums[left] ** 2)
-            left ++
-        } else {
-            squarednums.unshift(nums[right] ** 2)
-            right --
-        }
+    let arr = []
+    for(let i=0; i<nums.length; i++){
+        arr.push(nums[i] ** 2)
     }
-    return squarednums
+    return arr.sort((a,b) => a-b)
 };
