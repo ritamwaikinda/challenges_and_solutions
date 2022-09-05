6abce68906534980cba682931254c489b2b6534c
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2022-09-04 20:15:31 -0400

    Time: 205 ms (16.55%), Space: 48.9 MB (29.96%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,8 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    for(let i=0; i<nums.length; i++){
-        nums[i] = nums[i] ** 2
+    // for(let i=0; i<nums.length; i++){
+    //     nums[i] = nums[i] ** 2
+    // }
+    // return nums.sort((a,b) => a-b)
+    let squarednums = []
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right){
+        if (Math.abs(nums[left]) > Math.abs(nums[right])){
+            squarednums.unshift(nums[left] ** 2)
+            left ++
+        } else {
+            squarednums.unshift(nums[right] ** 2)
+            right --
+        }
     }
-    return nums.sort((a,b) => a-b)
+    return squarednums
 };
