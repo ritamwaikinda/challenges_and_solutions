32b8aaa43a040cfd0d0ca443ea94e633dd1ec81a
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2022-09-30 23:18:11 -0400

    Time: 102 ms (47.77%), Space: 42.5 MB (57.42%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -4,6 +4,7 @@
  */
 var longestCommonPrefix = function(strs) {
     let commonPrefix = "";
+    
     for(let i = 0; i < strs[0].length; i++){
         for(let j=0; j<strs.length; j++){
                 if(!strs[j][i]){
